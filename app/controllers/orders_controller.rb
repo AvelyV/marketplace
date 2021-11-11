311afdd868268ca81b2312e5c185a31c2e1d8f4e
AvelyV <avely.voore@gmail.com>
2021-11-11 15:56:17 +1000

    added stripe, success and cancel still to do

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,29 +2,22 @@ class OrdersController < ApplicationController
     def buy
         Stripe.api_key = "#{ENV['STRIPE_API_KEY']}"
 
+        listing = Listing.find(params[:listing_id])
+
         line_item = {
             price_data: {
               currency: 'aud',
               product_data: {
-                name: 'T-shirt'
+                name: listing.title
               },
-              unit_amount: 2000
+              unit_amount: listing.price
             },
             quantity: 1
           }
-          
+
         session = Stripe::Checkout::Session.create({
             payment_method_types: ['card'],
-            line_items: [{
-              price_data: {
-                currency: 'aud',
-                product_data: {
-                  name: 'T-shirt'
-                },
-                unit_amount: 2000
-              },
-              quantity: 1
-            }],
+            line_items: [line_item],
             mode: 'payment',
             # These placeholder URLs will be replaced in a following step.
             success_url: success_url,
@@ -35,7 +28,8 @@ class OrdersController < ApplicationController
     end
 
     def success
-
+        # reduce qty on the listing by qty bought
+        
     end
 
     def cancel
