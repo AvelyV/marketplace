76717ee070511962de6f72a800e6a06e2e18af2c
AvelyV <avely.voore@gmail.com>
2021-11-17 11:58:43 +1000

    stripe now works 🥳

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,64 +1,50 @@
 class OrdersController < ApplicationController
-    def buy
-        Stripe.api_key = "#{ENV['STRIPE_API_KEY']}"
+  def buy
+    Stripe.api_key = (ENV['STRIPE_API_KEY']).to_s
 
-        listing = Listing.find(params[:listing_id])
+    listing_in = Listing.find(params[:listing_id])
 
-        line_item = {
-            price_data: {
-              currency: 'aud',
-              product_data: {
-                name: listing.title
-              },
-              unit_amount: (listing.price * 100).to_i
-            },
-            quantity: 1
-          }
+    line_item = {
+      price_data: {
+        currency: 'aud',
+        product_data: {
+          name: listing_in.title
+        },
+        unit_amount: (listing_in.price * 100).to_i
+      },
+      quantity: 1
+    }
 
-        session = Stripe::Checkout::Session.create({
-            payment_method_types: ['card'],
-            line_items: [line_item],
-            mode: 'payment',
-            # These placeholder URLs will be replaced in a following step.
-            success_url: success_url + "?session_id={USER_SESSION_ID}",
-            cancel_url: cancel_url + "?session_id={USER_SESSION_ID}"
-          })
-        # respond_to do |format|
-        #     format.js
-        # end
+    session = Stripe::Checkout::Session.create({
+                                                 payment_method_types: ['card'],
+                                                 line_items: [line_item],
+                                                 mode: 'payment',
+                                                 # These placeholder URLs will be replaced in a following step.
+                                                 success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
+                                                 cancel_url: cancel_url + "?session_id={CHECKOUT_SESSION_ID}",
+                                                 client_reference_id: listing_in.id
+                                               })
+    # respond_to do |format|
+    #     format.js
+    # end1)
 
-        @listing = Listing.find_by(stripe_product_id: line_item.price.listing)
-              
-        @listing.update(@listing.decrement!(:qty, 1))
-        redirect_to session.url
+    redirect_to session.url
+  end
+
+  def success
+    session = Stripe::Checkout::Session.retrieve("#{params[:session_id]}")
+    # reduce qty on the listing by qty bought
+    if params[:session_id].present?
+      p params
+      @listing = Listing.find_by(id: session[:client_reference_id])
+      @listing.decrement!(:qty, 1)
+      redirect_to listings_path, notice: "Payment Seccessful"
+    else
+      redirect_to cancel_url
     end
+  end
 
-    def success
-        # reduce qty on the listing by qty bought
-        if params[:session_id].present?
-
-            @session_with_expand = Stripe::Checkout::Session.list_line_items(params[:session_id])
-            @session_with_expand.line_items.data.each do |line_item|
-              @listing = Listing.find_by(stripe_product_id: line_item.description)
-              
-              @listing.decrement!(:qty, 1)
-              # @listing.update(sales_count: @listing.sales_count += 1)
-              # @listing.increment!(:sales_count)
-
-              raise
-            end
-          
-          # else
-          #   redirect_to cancel_url
-        end
-    end
-        
-    def success
-
-        redirect_to listings_path, notice: "Payment Seccessful"
-    end
-
-    def cancel
-      redirect_to listings_path, alert: "Something went wrong, try again"
-    end
+  def cancel
+    redirect_to listings_path, alert: "Something went wrong, try again"
+  end
 end
