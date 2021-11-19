607484beb6b86019162abe602669e6b0d084b38d
AvelyV <avely.voore@gmail.com>
2021-11-19 21:31:21 +1000

    created messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,27 @@
+class MessagesController < ApplicationController
+  before_action :find_conversation
+
+  def index
+    @messages = @conversation.messages
+    @message = @conversation.messages.new
+  end
+
+  def new
+    @message = @conversation.messages.new
+  end
+
+  def create
+    @message = @conversation.messages.new(message_params)
+    redirect_to conversation_messages_path(@conversation) if @message.save
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
+
+  def find_conversation
+    @conversation = Conversation.find(params[:conversation_id])
+  end
+end
